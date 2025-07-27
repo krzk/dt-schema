dfb659e1345b1928bb58bc2594d752e9e88ea1c1
Krzysztof Kozlowski <krzk@kernel.org>
2025-07-27 16:36:11 +0200

    dts_style: test: Split testing node naming convention
    Separately test node naming conventions of nodes with and without unit
    address, because further node order rules would interfere.
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>

--- test/test-dts-style.py
+++ test/test-dts-style.py
@@ -16,28 +16,38 @@ class TestDTMetaSchema(unittest.TestCase):
 
     def test_nodename(self):
         expected = [
-            ['Whitespace error', '\tinterrupt-controller-3  {', 38],
-            ['Whitespace error', '\tintc_4: interrupt-controller-4  {', 42],
-            ['Label: use underscores instead of hyphens', '\tintc-5: interrupt-controller-5  {', 46],
-            ['Whitespace error', '\tintc-5: interrupt-controller-5  {', 46],
-            ['Whitespace error', '\tintc_6:  interrupt-controller-6 {', 50],
-            ['Whitespace error', '\tintc_7:interrupt_controller-7 {', 54],
-            ['Node name: use hyphens instead of underscores', '\tintc_7:interrupt_controller-7 {', 54],
-            ['Label: only lowercase letters', '\tintC_8: interrupt-controller-8 {', 58],
-            ['Node name: only lowercase letters', '\tintc_9: interrupt-controlleR-9 {', 62],
-            ['Node name: only lowercase letters', '\tinterrupt-controlleR-10 {', 66],
-            ['Whitespace error', '\tinterrupt-controller@3  {', 70],
-            ['Whitespace error', '\tintc_4_2: interrupt-controller@4  {', 75],
-            ['Label: use underscores instead of hyphens', '\tintc-5_2: interrupt-controller@5  {', 80],
-            ['Whitespace error', '\tintc-5_2: interrupt-controller@5  {', 80],
-            ['Whitespace error', '\tintc_6_2:  interrupt-controller@6 {', 85],
-            ['Whitespace error', '\tintc_7_2:interrupt_controller@7 {', 90],
-            ['Node name: use hyphens instead of underscores', '\tintc_7_2:interrupt_controller@7 {', 90],
-            ['Label: only lowercase letters', '\tintC_8_2: interrupt-controller@8 {', 95],
-            ['Node name: only lowercase letters', '\tintc_9_2: interrupt-controlleR@9 {', 100],
-            ['Unit address: avoid leading "0"', '\tinterrupt-controller@0a {', 105],
+            ['Whitespace error', '\tinterrupt-controller-3  {', 23],
+            ['Whitespace error', '\tintc_4: interrupt-controller-4  {', 27],
+            ['Label: use underscores instead of hyphens', '\tintc-5: interrupt-controller-5  {', 31],
+            ['Whitespace error', '\tintc-5: interrupt-controller-5  {', 31],
+            ['Whitespace error', '\tintc_6:  interrupt-controller-6 {', 35],
+            ['Whitespace error', '\tintc_7:interrupt_controller-7 {', 39],
+            ['Node name: use hyphens instead of underscores', '\tintc_7:interrupt_controller-7 {', 39],
+            ['Label: only lowercase letters', '\tintC_8: interrupt-controller-8 {', 43],
+            ['Nodes do not look sorted alphanumerically', '\tintC_8: interrupt-controller-8 {', 43],
+            ['Node name: only lowercase letters', '\tintc_9: interrupt-controlleR-9 {', 47],
+            ['Nodes do not look sorted alphanumerically', '\tintc_9: interrupt-controlleR-9 {', 47],
+            ['Node name: only lowercase letters', '\tinterrupt-controlleR-10 {', 51],
+            ['Nodes do not look sorted alphanumerically', '\tinterrupt-controlleR-10 {', 51],
         ]
         style = dtschema.DtsStyle(os.path.join(basedir, 'style/nodename.dts'))
+        style.check_dts()
+        self.assertListEqual(style.warnings, expected)
+
+    def test_nodename_unitaddr(self):
+        expected = [
+            ['Whitespace error', '\tinterrupt-controller@ff3  {', 30],
+            ['Whitespace error', '\tintc_4_2: interrupt-controller@ff4  {', 35],
+            ['Label: use underscores instead of hyphens', '\tintc-5_2: interrupt-controller@ff5  {', 40],
+            ['Whitespace error', '\tintc-5_2: interrupt-controller@ff5  {', 40],
+            ['Whitespace error', '\tintc_6_2:  interrupt-controller@ff6 {', 45],
+            ['Whitespace error', '\tintc_7_2:interrupt_controller@ff7 {', 50],
+            ['Node name: use hyphens instead of underscores', '\tintc_7_2:interrupt_controller@ff7 {', 50],
+            ['Label: only lowercase letters', '\tintC_8_2: interrupt-controller@ff8 {', 55],
+            ['Node name: only lowercase letters', '\tintc_9_2: interrupt-controlleR@ff9 {', 60],
+            ['Unit address: avoid leading "0"', '\tinterrupt-controller@0ffa {', 65],
+        ]
+        style = dtschema.DtsStyle(os.path.join(basedir, 'style/nodename-unitaddr.dts'))
         style.check_dts()
         self.assertListEqual(style.warnings, expected)
 
